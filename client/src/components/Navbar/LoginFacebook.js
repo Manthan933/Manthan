3d0f71ec4eb8ff410b9e4e0d75f2a13d1dcbc192
osamamagdy <59124937+osamamagdy@users.noreply.github.com>
2021-03-23 23:09:00 +0200

    Implement jwt in google login

--- client/src/components/Navbar/LoginFacebook.js
+++ client/src/components/Navbar/LoginFacebook.js
@@ -1,5 +1,18 @@
 import React from "react";
 import FacebookLogin from "react-facebook-login";
+const jwt = require('jsonwebtoken');
+
+
+
+
+/**Age of the token in seconds*/
+const mymaxAge = 1 * 24 * 60 * 60;
+const createToken = (email, username, usertype) => {
+    /**ًWhen this is released to production, 'Manthan Secret' should be not uploaded to public' */
+    return jwt.sign({ email, username, usertype }, 'Manthan Secret', { expiresIn: mymaxAge });
+}
+
+
 
 export default function LoginFacebook(props) {
   const { appId, setAuth, setUser } = props;
@@ -10,10 +23,15 @@ export default function LoginFacebook(props) {
         username: response.name,
         email: response.email,
         image: response.picture.data.url,
+        userType : "user"
       };
       localStorage.setItem("user", JSON.stringify({ user }));
       setUser(user);
       setAuth(true);
+      const token = createToken(user.email , user.username , user.userType);
+      const expireDate = new Date(Date.now() + mymaxAge * 1000);
+      document.cookie = `jwt=${token}; expires=${expireDate}`;
+
     } else {
       alert("Failed to log in");
     }
